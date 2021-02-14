895eebb882d63939f1c49cf522ab655d4cb7cd9f
AntonioRecaldeRusso <antonio@deckardtech.com>
2021-02-13 23:08:09 -0800

    initial commit

---
+++ color_getter.py
@@ -0,0 +1,15 @@
+def get_color():
+    """This function asks the user for a color name, and returns a color code
+        1) Ask the user for a color
+        2) Read colors from colors.json
+        3a) If color found, print: <color-name>: <color-value>. E.g.:
+            For "aliceblue", print:
+                aliceblue: [240, 248, 255, 1]
+        3b) If color not found: print nothing
+        4) return color value (return None if color not found)
+    """
+    pass
+
+
+if __name__ == "__main__":
+    get_color()
---
+++ color_setter.py
@@ -0,0 +1,11 @@
+def set_color():
+    """This function allows the user to update a color
+        1) ask user for a color name
+        2) ask user for a color value
+        3) overwrite colors.json file, adding the new color name and value
+    """
+    pass
+
+
+if __name__ == "__main__":
+    set_color()
